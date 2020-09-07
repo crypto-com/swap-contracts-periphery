766569e4fe4c638db16725ae3a59ee35c7629cbb
ceruleanhu <hulandan123@gmail.com>
2020-09-08 00:45:39 +0800

    Issue SWAP-27: rename

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
-const UniswapV2Router02 = artifacts.require("UniswapV2Router02");
+const CroDefiSwapRouter02 = artifacts.require("CroDefiSwapRouter02");
 const fs = require("fs");
 
 module.exports = (deployer, network, account) => {
 	const WETH = JSON.parse(fs.readFileSync(`./WETH/contract-${network}.json`, "utf8"));
-	const coreFactoryJSON = JSON.parse(fs.readFileSync("./swap-contracts-core/build/contracts/UniswapV2Factory.json", "utf8"));
+	const coreFactoryJSON = JSON.parse(fs.readFileSync("./swap-contracts-core/build/contracts/CroDefiSwapFactory.json", "utf8"));
 	const coreFactoryNetwork = Object.keys(coreFactoryJSON.networks)[0];
 	const coreFactoryAddress = coreFactoryJSON.networks[coreFactoryNetwork].address;
 
-	deployer.deploy(UniswapV2Router02, coreFactoryAddress, WETH.address);
+	deployer.deploy(CroDefiSwapRouter02, coreFactoryAddress, WETH.address);
 };
--- test/shared/fixtures.ts
+++ test/shared/fixtures.ts
@@ -4,16 +4,12 @@ import { deployContract } from 'ethereum-waffle'
 
 import { expandTo18Decimals } from './utilities'
 
-import UniswapV2Factory from '@uniswap/v2-core/build/UniswapV2Factory.json'
-import IUniswapV2Pair from '@uniswap/v2-core/build/IUniswapV2Pair.json'
+import CroDefiSwapFactory from '../../swap-contracts-core/build/contracts/CroDefiSwapFactory.json'
+import ICroDefiSwapPair from '../../swap-contracts-core/build/contracts/ICroDefiSwapPair.json'
 
 import ERC20 from '../../build/ERC20.json'
 import WETH9 from '../../build/WETH9.json'
-import UniswapV1Exchange from '../../build/UniswapV1Exchange.json'
-import UniswapV1Factory from '../../build/UniswapV1Factory.json'
-import UniswapV2Router01 from '../../build/UniswapV2Router01.json'
-import UniswapV2Migrator from '../../build/UniswapV2Migrator.json'
-import UniswapV2Router02 from '../../build/UniswapV2Router02.json'
+import CroDefiSwapRouter02 from '../../build/CroDefiSwapRouter02.json'
 import RouterEventEmitter from '../../build/RouterEventEmitter.json'
 
 const overrides = {
@@ -25,14 +21,10 @@ interface V2Fixture {
   token1: Contract
   WETH: Contract
   WETHPartner: Contract
-  factoryV1: Contract
   factoryV2: Contract
-  router01: Contract
   router02: Contract
   routerEventEmitter: Contract
   router: Contract
-  migrator: Contract
-  WETHExchangeV1: Contract
   pair: Contract
   WETHPair: Contract
 }
@@ -44,34 +36,19 @@ export async function v2Fixture(provider: Web3Provider, [wallet]: Wallet[]): Pro
   const WETH = await deployContract(wallet, WETH9)
   const WETHPartner = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)])
 
-  // deploy V1
-  const factoryV1 = await deployContract(wallet, UniswapV1Factory, [])
-  await factoryV1.initializeFactory((await deployContract(wallet, UniswapV1Exchange, [])).address)
-
   // deploy V2
-  const factoryV2 = await deployContract(wallet, UniswapV2Factory, [wallet.address])
+  const factoryV2 = await deployContract(wallet, CroDefiSwapFactory, [wallet.address])
 
   // deploy routers
-  const router01 = await deployContract(wallet, UniswapV2Router01, [factoryV2.address, WETH.address], overrides)
-  const router02 = await deployContract(wallet, UniswapV2Router02, [factoryV2.address, WETH.address], overrides)
+  const router02 = await deployContract(wallet, CroDefiSwapRouter02, [factoryV2.address, WETH.address], overrides)
 
   // event emitter for testing
   const routerEventEmitter = await deployContract(wallet, RouterEventEmitter, [])
 
-  // deploy migrator
-  const migrator = await deployContract(wallet, UniswapV2Migrator, [factoryV1.address, router01.address], overrides)
-
-  // initialize V1
-  await factoryV1.createExchange(WETHPartner.address, overrides)
-  const WETHExchangeV1Address = await factoryV1.getExchange(WETHPartner.address)
-  const WETHExchangeV1 = new Contract(WETHExchangeV1Address, JSON.stringify(UniswapV1Exchange.abi), provider).connect(
-    wallet
-  )
-
   // initialize V2
   await factoryV2.createPair(tokenA.address, tokenB.address)
   const pairAddress = await factoryV2.getPair(tokenA.address, tokenB.address)
-  const pair = new Contract(pairAddress, JSON.stringify(IUniswapV2Pair.abi), provider).connect(wallet)
+  const pair = new Contract(pairAddress, JSON.stringify(ICroDefiSwapPair.abi), provider).connect(wallet)
 
   const token0Address = await pair.token0()
   const token0 = tokenA.address === token0Address ? tokenA : tokenB
@@ -79,21 +56,17 @@ export async function v2Fixture(provider: Web3Provider, [wallet]: Wallet[]): Pro
 
   await factoryV2.createPair(WETH.address, WETHPartner.address)
   const WETHPairAddress = await factoryV2.getPair(WETH.address, WETHPartner.address)
-  const WETHPair = new Contract(WETHPairAddress, JSON.stringify(IUniswapV2Pair.abi), provider).connect(wallet)
+  const WETHPair = new Contract(WETHPairAddress, JSON.stringify(ICroDefiSwapPair.abi), provider).connect(wallet)
 
   return {
     token0,
     token1,
     WETH,
     WETHPartner,
-    factoryV1,
     factoryV2,
-    router01,
     router02,
     router: router02, // the default router, 01 had a minor bug
     routerEventEmitter,
-    migrator,
-    WETHExchangeV1,
     pair,
     WETHPair
   }
