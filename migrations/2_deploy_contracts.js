8e8ffce26b1f5e60ceebd71595b35f892fe3774b
ceruleanhu <hulandan123@gmail.com>
2020-09-08 01:31:26 +0800

    Issue SWAP-27: fix deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 module.exports = (deployer, network, account) => {
 	const WETH = JSON.parse(fs.readFileSync(`./WETH/contract-${network}.json`, "utf8"));
-	const coreFactoryJSON = JSON.parse(fs.readFileSync("./swap-contracts-core/build/contracts/UniswapV2Factory.json", "utf8"));
+	const coreFactoryJSON = JSON.parse(fs.readFileSync("./swap-contracts-core/build/contracts/CroDefiSwapFactory.json", "utf8"));
 	const coreFactoryNetwork = Object.keys(coreFactoryJSON.networks)[0];
 	const coreFactoryAddress = coreFactoryJSON.networks[coreFactoryNetwork].address;
 
