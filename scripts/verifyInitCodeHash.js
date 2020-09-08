9a142889f16c3d923ac93b034a3e20826bb6f850
Franco NG <franco.ng@crypto.com>
2020-09-08 22:38:29 +0800

    Issue SWAP-32: Update script

--- scripts/verifyInitCodeHash.js
+++ scripts/verifyInitCodeHash.js
@@ -10,6 +10,6 @@ const CroDefiSwapLibrary = fs.readFileSync(LIBRARY_PATH, "utf8");
 const initCodeHash = Web3.utils.keccak256("0x" + swapPairJSON.bytecode).split("x")[1];
 const initCodeHashInSol = CroDefiSwapLibrary.match(/([a-z0-9]){64}/g);
 if (initCodeHashInSol.length === 0 || (initCodeHash !== initCodeHashInSol[0])) {
-	throw `[updateInitCodeHash] initCodeHash not match! ${initCodeHashInSol} !== ${initCodeHashInSol}`;
+	throw `[verifyInitCodeHash] initCodeHash not match! ${initCodeHashInSol[0]} !== ${initCodeHash}`;
 }
-console.log(`[updateInitCodeHash] initCodeHash matches ${initCodeHash}`);
+console.log(`[verifyInitCodeHash] initCodeHash matches ${initCodeHash}`);
