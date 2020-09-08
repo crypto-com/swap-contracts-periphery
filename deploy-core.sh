3515d97d8492ef1b708d50e5aef1d9e126b44355
Franco NG <franco.ng@crypto.com>
2020-09-08 22:54:34 +0800

    Issue SWAP-32: Use truffle to compile

--- deploy-core.sh
+++ deploy-core.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 cd swap-contracts-core
 yarn install
-yarn compile # Waffle
-yarn truffle-compile
-yarn replace-factory
+yarn clean
 ./node_modules/.bin/truffle migrate --reset --network $1
