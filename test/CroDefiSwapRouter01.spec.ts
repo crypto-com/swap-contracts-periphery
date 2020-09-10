8341e39c55b4695503e722e80200674521117797
ceruleanhu <hulandan123@gmail.com>
2020-09-10 10:34:23 +0800

    Issue SWAP-33: fix gas test..

--- test/CroDefiSwapRouter01.spec.ts
+++ test/CroDefiSwapRouter01.spec.ts
@@ -368,7 +368,7 @@ describe('CroDefiSwapRouter{01,02}', () => {
           const receipt = await tx.wait()
           expect(receipt.gasUsed).to.eq(
             {
-              [RouterVersion.CroDefiSwapRouter01]: 105376,
+              [RouterVersion.CroDefiSwapRouter01]: 105398,
               [RouterVersion.CroDefiSwapRouter02]: 105420
             }[routerVersion as RouterVersion]
           )
